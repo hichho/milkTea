a12af274432599e105796655673edad502986c50
hichho <52339006+hichho@users.noreply.github.com>
2020-12-21 18:03:48 +0800

    暂存

--- src/components/Body2/index.js
+++ src/components/Body2/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/swiper-bundle.css';
+// import 'swiper/swiper-bundle.css';
 import SwiperCore, {
   Navigation,
   Pagination,
@@ -11,28 +11,14 @@ import SwiperCore, {
 import 'swiper/components/navigation/navigation.less';
 import 'swiper/components/pagination/pagination.less';
 import 'swiper/components/scrollbar/scrollbar.less';
-import less from './index.less';
+import './index.less';
 import Flex from '@/components/Flex';
 // import 'swiper/swiper.scss';
 
 SwiperCore.use([Navigation, Pagination, Scrollbar, A11y, Autoplay]);
 
 export default () => {
-  const renderCustom = (swiper, current, total) => {
-    console.log(swiper, current, total);
-
-    let paginationHtml = '';
-    let text = ['等级专属券', '免邮特权', '生日礼券'];
-    for (let i = 1; i <= total; i++) {
-      if (i == current) {
-        paginationHtml = <div>1</div>;
-      } else {
-        paginationHtml = <div>12</div>;
-      }
-    }
-    console.log(paginationHtml);
-    return paginationHtml;
-  };
+  let mySwiper;
 
   return (
     <div
@@ -44,28 +30,46 @@ export default () => {
       }}
     >
       <Swiper
-        // simulateTouch={true}
         loop={true}
         autoplay={{
           autoPlay: true,
-          delay: 1500,
+          delay: 150000,
           autoplayDisableOnInteraction: false,
         }}
         pagination={{
+          el: '.swiper-pagination',
           clickable: true,
-          // renderCustom: function () {
-          //   return <span>fsfs</span>
-          // },
-          renderBullet: function(index, className) {
-            console.log(index, className);
-            console.log('<span class="' + 'special' + '">' + index + '</span>');
-            return '<span class="' + 'special' + '">' + index + '</span>';
+          type: 'custom',
+          pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+            //自定义分页类型
+            type: 'custom',
+            //自定义分页
+            renderBullet: function(index, className) {
+              console.log(index);
+              if (index === 1) {
+                return '<div class="swiper-pagination-bullet ubolt-black"></div>';
+              } else {
+                return '<div class="swiper-pagination-bullet ubolt-white"></div>';
+              }
+            },
           },
+          // renderCustom: function(swiper, current, total) {
+          //   var customPaginationHtml = "";
+          //   for (var i = 0; i < total; i++) {
+          //     //判断哪个分页器此刻应该被激活
+          //     if (i == current - 1) {
+          //       customPaginationHtml +=
+          //         '<span class="swiper-pagination-customs swiper-pagination-customs-active"></span>';
+          //     } else {
+          //       customPaginationHtml +=
+          //         '<span class="swiper-pagination-customs"></span>';
+          //     }
+          //   }
+          //   return '<span class="swiperPag">'+customPaginationHtml+'</span>';
+          // }
         }}
-        onSwiper={swiper => {
-          console.log(swiper);
-        }}
-        onSlideChange={() => console.log('slide change')}
       >
         <SwiperSlide virtualIndex={'1'}>
           <img
@@ -88,6 +92,11 @@ export default () => {
             style={{ width: '100%', height: '500px', objectFit: 'cover' }}
           />
         </SwiperSlide>
+        {/*<div className="swiper-pagination"></div>*/}
+        <div className="swiper-pagination ubolt-swiper-pagination">
+          <span className="swiper-pagination-bullet ubolt-black"></span>
+          <span className="swiper-pagination-bullet ubolt-white"></span>
+        </div>
       </Swiper>
 
       <Flex>测试</Flex>
